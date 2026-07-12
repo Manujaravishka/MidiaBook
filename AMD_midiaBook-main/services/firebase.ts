07bc904e97bd07fc6dad89137d5e38a9397b4b66
Manujaravishka <manujaravishka2004@gmail.com>
2026-07-12 15:53:48 +0530

    fixs firebase

--- AMD_midiaBook-main/services/firebase.ts
+++ AMD_midiaBook-main/services/firebase.ts
@@ -1,19 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, } from "firebase/auth";
-import {getFirestore} from "firebase/firestore"
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvQGKuhM1pYVAe9Zbc03jDqhahLx0MmP0",
-  authDomain: "media-final-a0c45.firebaseapp.com",
-  projectId: "media-final-a0c45",
-  storageBucket: "media-final-a0c45.firebasestorage.app",
-  messagingSenderId: "746108505316",
-  appId: "1:746108505316:web:9aa840c0f4079c619c30f1"
+  apiKey: "AIzaSyC5hyW3l064LrbjhS91SDVcNaSM67YPTX0",
+  authDomain: "midiabook.firebaseapp.com",
+  projectId: "midiabook",
+  storageBucket: "midiabook.firebasestorage.app",
+  messagingSenderId: "1016977765660",
+  appId: "1:1016977765660:web:f37fb1a913ba4cbc0136d9",
+  measurementId: "G-TKDHJEBMVQ"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
